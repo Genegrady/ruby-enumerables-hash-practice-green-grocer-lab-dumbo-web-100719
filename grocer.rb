1a2c553343954bdb1109be6957a5e8f22015bdde
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-30 15:11:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,5 +42,5 @@ end
 def checkout(cart, coupons)
   new_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(new_cart, coupons)
-  applied_clearance =
+  applied_clearance = apply_clearance()
 end
