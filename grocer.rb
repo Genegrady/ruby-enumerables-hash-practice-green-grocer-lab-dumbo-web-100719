1b2571618aceac4ab05792d29fb81c9181432758
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-30 14:03:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+ new_hash = {}
+ cart.each do |element_hash|
+  element_name = element_hash.keys[0]
+  element_stats = element_hash.values[0]
+  
+    if new_hash.has_key?(element_name)
+      new_hash[element_name][:count] += 1 
+    else
+      new_hash[element_name] = {
+        count: 1,
+        price: element_stats[:price], 
+        clearance: element_stats[:clearance],
+      }
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
